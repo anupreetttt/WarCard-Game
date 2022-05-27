3adf7fae95a28c9d19ec8af34cdaa4d7deba9339
anupreetttt <anupreetpaulkar@gmail.com>
2022-05-27 01:11:45 -0500

    Initial Commit

---
+++ iOSdev/iOSdevApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSdevApp.swift
+//  iOSdev
+//
+//  Created by Anupreet Paulkar on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSdevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
